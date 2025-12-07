1e0428ebdaa6bb9f8b38a768cdf33db70996fe62
Foffe Lili <lilifoffe10@gmail.com>
2025-12-07 06:07:53 +0100

    Add the collapsible block

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const collapsibles = document.querySelectorAll('.collapsible');
+collapsibles.forEach((collapsible) => {
+    collapsible.addEventListener('click', () => {
+        collapsible.classList.toggle('collapsible-expended'); });
+});
