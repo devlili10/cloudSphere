262a671e1d301c00e4a58a47a218e6654bef5f4c
Foffe Lili <lilifoffe10@gmail.com>
2025-12-11 03:46:17 +0100

    Feat: Build the footer component.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const collapsibles = document.querySelectorAll('.collapsible');
 collapsibles.forEach((collapsible) => {
     collapsible.addEventListener('click', () => {
-        collapsible.classList.toggle('collapsible-expended'); });
+        collapsible.classList.toggle('collapsible-expanded'); });
 });
+
+ document.getElementById("year").textContent = new Date().getFullYear();
